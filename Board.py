ffc2ae2bb739e2a77b352e35709106132a0558af
Anthony Perugini <Anthony.r.perugini@gmail.com>
2021-07-06 11:03:14 -0400

    value functions are hard.  moving testing over to play.py

--- Board.py
+++ Board.py
@@ -1,38 +1,70 @@
 import chess
 import numpy as np
+from collections import defaultdict
 
 class Board(chess.Board):
 
+    plus_minus = {True: 1, False: -1}
+    memo = defaultdict(int)
     values = {'K': 900, 'Q': 90, 'R': 50, 'B': 30, 'N': 30, 'P': 10, \
               'k': -900, 'q': -90, 'r': -50, 'b': -30, 'n': -30, 'p': -10}
 
-    def value(self, mobility_weight=1, center_control_weight=1):
-        val = 0
-        piece_positions = self.fen().split(' ')[0]
-        for piece in piece_positions:
-            if piece not in Board.values.keys():
-                continue
-            val += Board.values[piece] 
+    @classmethod
+    def reset_memo(cls):
+        cls.memo = defaultdict(int)
 
-        # piece mobility
-        val += self.legal_moves.count() * mobility_weight
 
-        # king safety
-        
-        
-        # center control
-        for square in (chess.D4, chess.D5, chess.E4, chess.E5):
-            attackers = self.attackers(self.turn, square)
-            num_attackers = len(attackers)
-            val += num_attackers * center_control_weight
+    def value(self, mobility_weight=0.5, center_control_weight=0.5):
 
-        # pawn structure
+        if not Board.memo[self.fen()]:
+            val = 0
+            pieces = self.fen().split(' ')[0]
+            for piece in pieces:
+                if piece not in Board.values.keys():
+                    continue
+                val += Board.values[piece] 
 
-        return val
+            # piece mobility for both players
+            piece_mobility = self.legal_moves.count() * mobility_weight 
+            val += piece_mobility * Board.plus_minus[self.turn]
+            self.turn = not self.turn
+            piece_mobility = self.legal_moves.count() * mobility_weight 
+            val += piece_mobility * Board.plus_minus[self.turn]
+            self.turn = not self.turn
+            
+            # king safety
+            
+            
+            # center control
+            center_control = 0
+
+            for square in (chess.D4, chess.D5, chess.E4, chess.E5):
+                attackers = self.attackers(self.turn, square)
+                num_attackers = len(attackers)
+                center_control = num_attackers * center_control_weight 
+
+            val += center_control * Board.plus_minus[self.turn]
+
+            self.turn = not self.turn
+
+            for square in (chess.D4, chess.D5, chess.E4, chess.E5):
+                attackers = self.attackers(self.turn, square)
+                num_attackers = len(attackers)
+                center_control = num_attackers * center_control_weight 
+
+            val += center_control * Board.plus_minus[self.turn]
+
+            self.turn = not self.turn
+
+            # pawn structure
+
+            Board.memo[self.fen()] = val
+
+        return Board.memo[self.fen()]
 
     
     @classmethod
-    def minimax(cls, board, depth, last_move=None): # returns a (value, best_move) tuple
+    def minimax(cls, board, depth, a, b, last_move=None):
 
         if depth == 0 or board.outcome():
             return board.value(), []
@@ -41,41 +73,50 @@ class Board(chess.Board):
             value = float('-inf')
             for move in board.legal_moves:
                 board.push(move)
-                cur_val, cur_path = Board.minimax(board, depth - 1, move)
-                if value < cur_val:
+                cur_val, cur_path = Board.minimax(board, depth - 1, a, b, move)
+                if cur_val > value:
                     value = cur_val
                     best_path = cur_path
                 board.pop()
 
-            best_path.append(last_move)
-
-            return value, best_path
+                if value >= b:
+                    break
+                a = max(a, value)
 
         else:
             value = float('inf')
             for move in board.legal_moves:
                 board.push(move)
-                cur_val, cur_path = Board.minimax(board, depth - 1, move)
-                if value > cur_val:
+                cur_val, cur_path = Board.minimax(board, depth - 1, a, b, move)
+                if cur_val < value:
                     value = cur_val
                     best_path = cur_path
                 board.pop()
 
+                if value <= a:
+                    break
+                b = min(b, value)
+
+        if last_move:
             best_path.append(last_move)
 
-            return value, best_path
+        return value, best_path
                 
 
 if __name__ == '__main__':
+
+    DEPTH = 4
+    MAXVAL = float('inf')
+    MINVAL = float('-inf')
+
     board = Board('rnbqkbnr/pp1ppppp/8/2p5/3P4/5N2/PPP1PPPP/RNBQKB1R w KQkq - 1 2')
 
-    for _ in range(10):
-        value, best_path = Board.minimax(board, 3)
-        best_move = best_path[-2]
+    for _ in range(20):
+        value, best_path = Board.minimax(board, DEPTH, MINVAL, MAXVAL)
+        best_move = best_path[-1]
         board.push(best_move)
-
         print(board)
-        print(f'{value=}')
+        print(f'{board.value()=}')
         print()
 
         
--- play.py
+++ play.py
@@ -1,6 +1,18 @@
 #! /home/anthony/miniconda3/envs/chess/bin/python
-
 from Board import Board
 
+DEPTH = 4
+MAXVAL = float('inf')
+MINVAL = float('-inf')
+
 board = Board()
-print(board)
+
+
+while board.outcome() is None:
+    value, best_moves = Board.minimax(board, DEPTH, MINVAL, MAXVAL)
+    best_move = best_moves[-1]
+    board.push(best_move)
+    print(board)
+    print(f'{board.value()=}\n')
+
+
